f53c415e70d94e71ba153658e1041c2e0b2c227d
Aayush Kuinkel <codewithaayush007@gmail.com>
2025-12-30 22:21:55 +0545

    initial commit

---
+++ frontend/app/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <div className="text-center">
+        <h1 className="text-2xl">Hello World</h1>
+      </div>
+    </>
+  );
+};
+
+export default page;
