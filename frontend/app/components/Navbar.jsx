4a2d007e6e2f9064b4d1edfa7d0734cb68fc2164
Aayush Kuinkel <codewithaayush007@gmail.com>
2025-12-31 08:03:58 +0545

    header update add landing

--- frontend/app/components/Navbar.jsx
+++ frontend/app/components/Navbar.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
     <>
-      <div className='p-3'>
-          <h1 className='text-xl font-bold'>UjjyaloHoodie</h1>
-          <div className='flex justify-between items-center mt-2'>
-              <div className='flex justify-start gap-2'>
-                <h2 className='text-xs flex gap-2 font-semibold'>All
-                  <span className='text-gray-400'>20</span>
-                </h2>
-                <h2 className='text-xs flex gap-2 font-semibold'>Stores
-                  <span className='text-gray-400'>4</span>
-                </h2>
-              </div>
-              <div>
-                <h2 className='text-xs flex gap-2 font-semibold'>Cart
-                  <span className='text-gray-400'>00</span>
-                </h2>
-              </div>
+      <div className="p-3">
+        <h1 className="text-sm font-bold">UjjyaloHoodie</h1>
+
+        <div className="flex justify-between items-center mt-3">
+          <div className="flex gap-3">
+            <h2 className="text-[10px] flex gap-1 font-semibold">
+              All <span className="text-gray-400">20</span>
+            </h2>
+            <h2 className="text-[10px] flex gap-1 font-semibold">
+              Stores <span className="text-gray-400">4</span>
+            </h2>
           </div>
+
+          <h2 className="text-[10px] flex gap-1 font-semibold">
+            Cart <span className="text-gray-400">00</span>
+          </h2>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,13 +1,41 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Landing from "./components/ui/Landing";
 
 const page = () => {
   return (
     <>
-      <section className="bg-gray-200 h-screen relative">
-        <div className="bg-white w-[95%] md:w-100 fixed m-3 md:m-0 md:top-5 md:left-5">
-          <Navbar />
-        </div>
+      <section className="bg-gray-200/50 min-h-screen relative flex flex-col md:block">
+        <header>
+          {/* Navbar */}
+          <div className="bg-white w-[95%] md:w-100 mx-auto mt-3 md:fixed md:top-2 md:left-5 z-20">
+            <Navbar />
+          </div>
+
+          {/* Collections Bar */}
+          <div className="bg-white w-[250px] md:mt-3 ms-2.5 md:absolute md:top-2 md:right-5 z-10">
+            <div className="flex justify-between items-center">
+              {/* Left */}
+              <h2 className="text-[10px] font-semibold flex items-center gap-2 ms-3">
+                Collections
+                <span className="text-gray-400 font-normal">01 / 01</span>
+              </h2>
+
+              {/* Right box */}
+              <div className="w-8 h-8 border border-black flex items-center justify-center">
+                <span className="text-xs font-bold">M</span>
+              </div>
+            </div>
+          </div>
+        </header>
+
+        {/* Page Start */}
+        <main>
+          {/* Landing Section */}
+          <section className="absolute top-40 p-10">
+            <Landing />
+          </section>
+        </main>
       </section>
     </>
   );
